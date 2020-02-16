a41b11d4dda0cf3abfe71b603d1b53a0906ef58b
niv b <nivb52@gmail.com>
2020-02-16 16:19:35 +0200

    change logger

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const logger = require('morgan');
+const logger = (...txt) => console.log(...txt);
 const createError = require('http-errors');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -46,12 +46,11 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  req.app.get('env') === 'development' ? err : {};
 
   // render my error page
   res.status(err.status || 500);
-  console.log(err)
+  logger(err)
   res.send('ERROR')
 });
 
