6b9fa09736e0e4a76031e0a8ed70ec6b23e42b6e
niv b <nivb52@gmail.com>
2020-02-16 22:00:25 +0200

    errors route

--- server/routes/error.js
+++ server/routes/error.js
@@ -3,8 +3,8 @@ const router = new express.Router();
 const logger = (...txt) => console.log(...txt);
 
 router.get('/', (req, res) => {
-    const { error = 'Something went wrong' } = req.query;
-    res.send(error);
+    const { e  } = req.query || 'Something went wrong';
+    res.status(500, e).end();
   });
 
 
