c9d4a889c2469d4e95511f1f258b3e047ceddec0
niv b <nivb52@gmail.com>
2020-02-17 19:50:37 +0200

    use default error handling

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const logger = require('../services/logger')
+const logger = require('../services/logger');
 
 const createError = require('http-errors');
 const path = require('path');
@@ -7,8 +7,6 @@ const exphbs = require('express-handlebars');
 
 // APP
 const app = express();
-// DEBUG
-// app.use(logger('default','dev'));
 app.use(express.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'web', 'views'));
@@ -26,16 +24,13 @@ app.use(express.static(path.join(__dirname, 'web', 'public')));
 // ROUTES //
 // ================ //
 
-
 app.use('/', require('./routes/tweet'));
 
 app.use('/error', require('./routes/error'));
 
-
 // ==============================
-// DEFUALT ROUTES :
+// DEFUALT USE ROUTES :
 // ==============================
-
 
 // Don't create an error if favicon is requested
 app.use((req, res, next) => {
@@ -45,21 +40,20 @@ app.use((req, res, next) => {
   return next();
 });
 
-
-// // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(res.send(createError(404)));
 });
 
-
-// // other errors handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   req.app.get('env') === 'development' ? err : {};
-//   if (err.statusCode > 404) logger(err)  
-//   // render empty error page
-//   res.status(err.status || 500);
-// });
-
+// other errors handler
+app.use((err, req, res, next) => {
+  let msg = err.message || 'Something went wrong';
+  msg = encodeURIComponent(msg);
+  // set locals, only providing error in development
+  req.app.get('env') === 'development' ? err : {};
+  if (err.statusCode > 404) logger(err);
+  // render empty error page
+  res.status(err.status || 500).redirect('/error/?e=' + msg);
+});
 
 module.exports = app;
