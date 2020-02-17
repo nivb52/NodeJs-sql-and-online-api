080fefc94023c73f773fa12622461d8774595ced
niv b <nivb52@gmail.com>
2020-02-17 11:22:54 +0200

    adding options

--- services/logger.js
+++ services/logger.js
@@ -9,7 +9,7 @@ if (logLocal && (env === 'development' || env === 'dev')) {
   const Sentry = require('@sentry/node');
   logger = Sentry.init({
     dsn: process.env.SENTRY_ID,
-    maxBreadcrumbs: 30,
+    maxBreadcrumbs: 50,
     debug: true,
     integrations: function(integrations) {
       return integrations.concat(new Integrations.Dedupe());
