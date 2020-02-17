c0707e71534c4af8bce5fd96c46b73f181f50ef0
niv b <nivb52@gmail.com>
2020-02-17 11:29:41 +0200

    add validation tonew colmun isPending

--- services/tweet.validetor.js
+++ services/tweet.validetor.js
@@ -1,25 +1,29 @@
-const Joi = require("@hapi/joi");
-const logger = (...txt) => console.log(...txt);
+const Joi = require('@hapi/joi');
 
 // Schema:
 const schema = Joi.object().keys({
   comment: Joi.string()
-    .required()
-    .pattern(new RegExp("^[a-zA-Z0-9]{3,255}$")),
+    .required(),
+    // .pattern(new RegExp('^[a-zA-Z0-9]{3,255}$')),
   author: Joi.string()
     .optional()
-    .allow("")
+    .allow('')
     .strip()
     .max(255),
   tweet_account: Joi.string()
     .optional()
-    .allow("")
-    .email({ tlds: { allow: ["com", "il", "co.il"] } }),
+    .allow('')
+    .email({ tlds: { allow: ['com', 'il', 'co.il'] } }),
+  isPending: Joi.valid(0)
+    .valid(1)
+    .valid(false)
+    .valid(true)
+    .default(1)
+    .required(),
   createdAt: Joi.date().required(),
   updatedAt: Joi.date()
-    .min("now")
+    .min('now')
     .optional()
 });
 
-
-module.exports =  schema ;
+module.exports = schema;
