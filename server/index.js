bfcf717a5e202b85da90dfde6847b14c01f991c4
niv b <nivb52@gmail.com>
2020-02-17 11:28:36 +0200

    add logger

--- server/index.js
+++ server/index.js
@@ -2,5 +2,6 @@ const http = require("http");
 const app = require("./app");
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+const logger = require('../services/logger')
 
-server.listen(port, console.log("---> app listen on ", port));
+server.listen(port, logger("---> app listen on ", port));
