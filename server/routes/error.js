2ed926d0da0d3d78562d5c8fdb2776eb642793c0
niv b <nivb52@gmail.com>
2020-02-17 11:29:05 +0200

    add logger

--- server/routes/error.js
+++ server/routes/error.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = new express.Router();
-const logger = (...txt) => console.log(...txt);
+const logger = require('../../services/logger')
 
 router.get('/', (req, res) => {
     const { e  } = req.query || 'Something went wrong';
+    logger(e)
     res.status(500, e).end();
   });
 
