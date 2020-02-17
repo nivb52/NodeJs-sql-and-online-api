424fe801171b9da8f8905df5f07a1c86d50e25ab
niv b <nivb52@gmail.com>
2020-02-17 19:52:24 +0200

    rearrange

--- services/Tweet.service.js
+++ services/Tweet.service.js
@@ -11,7 +11,7 @@ const Model = models.Tweet;
 const EventEmitter = require('events').EventEmitter;
 const emitter = new EventEmitter();
 
-// INIT Queue :
+// INIT Queue (use this order):
 const tweetQ = createQueue();
 const isDequeueRunning = (state = true) => state;
 // let testQ = true; // for test the queue
@@ -20,16 +20,17 @@ _handleQueue();
 // Queue / Reducer TYPES :
 const POST_TWEET = 'postTweet';
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ==========================
 // TWEET FUNCTIONS / METHODS :
 // ==========================
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 async function validation(tweet) {
   tweet.isPending = 1; // default -> true
   tweet.createdAt = Date.now();
-
   const { error, value } = schema.validate(tweet);
-  if (error) return emitter.emit('error', error);
-  return value;
-  logger('Tweet.service 32: fn() saveAndPublish \n value:', value);
+  logger(' in Tweet.service saveAndPublish \n value:', value);
+  return { error, value };
 }
 
 async function postTweet({ comment, commentId }) {
@@ -47,12 +48,24 @@ async function postTweet({ comment, commentId }) {
       // testQ = false; //for test the queue
     } else {
       // ON PUBLISH -> UPDATE :
-      updatePending(commentId);
+      _updatePending(commentId);
     }
   });
 }
 
-async function updatePending(tweetId) {
+const Tweet = {};
+Tweet.Model = Model;
+Tweet.post = postTweet;
+Tweet.validation = validation;
+Tweet.emitter = emitter;
+
+module.exports = Tweet;
+
+// =======================
+// PRIVATE Fuctions / Methods
+// =======================
+
+async function _updatePending(tweetId) {
   try {
     const tweet = await Model.update(
       { isPending: 0 },
@@ -69,18 +82,6 @@ async function updatePending(tweetId) {
   logger('==> ' + data.text + '\n tweeted');
   return tweet;
 }
-
-const Tweet = {};
-Tweet.Model = Model;
-Tweet.post = postTweet;
-Tweet.validation = validation;
-Tweet.emitter = emitter;
-
-module.exports = Tweet;
-
-// =======================
-// PRIVATE Fuctions / Methods
-// =======================
 
 function _handleQueue() {
   while (!tweetQ.isEmpty) {
