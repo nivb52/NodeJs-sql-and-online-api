ebd0c81c6154fe192f3207055280761f2351c640
niv b <nivb52@gmail.com>
2020-02-17 12:41:07 +0200

    remove token from queue

--- services/queue.js
+++ services/queue.js
@@ -4,21 +4,10 @@ module.exports = { createQueue };
 
 function createQueue() {
   const queue = [];
-  const token = function() {
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
-    return (
-      "_" +
-      Math.random()
-        .toString(36)
-        .substr(2, 9)
-    );
-  };
 
   return {
     enqueue(item) {
-      queue.unshift({ item, token });
+      queue.unshift(item);
     },
 
     dequeue() {
